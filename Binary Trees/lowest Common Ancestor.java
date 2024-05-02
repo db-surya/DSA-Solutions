c6d45bd166d36552b0c0c4fe9fcf9ed0bba78bd4
db-surya <suryastarr123@gmail.com>
2024-05-02 23:42:28 +0530

    lowest common ancestor

---
+++ Binary Trees/lowest Common Ancestor.java
@@ -0,0 +1,10 @@
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root==null)  return null;
+        TreeNode leftNode = lowestCommonAncestor(root.left,p,q);
+        TreeNode rightNode = lowestCommonAncestor(root.right,p,q);
+        if((leftNode==p && rightNode == q) ||(leftNode==q && rightNode==p)) return root;
+        if(root == p || root==q) return root;
+        return leftNode==null?rightNode:leftNode;
+    }
+}
