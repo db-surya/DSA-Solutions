82cad376972df22d77c11fdbe10db0d3de4235fe
db-surya <suryastarr123@gmail.com>
2024-11-30 19:31:57 +0530

    coin change striver solution

--- Blind 75/Dynamic programming/Coin change.java
+++ Blind 75/Dynamic programming/Coin change.java
@@ -12,3 +12,34 @@ class Solution {
         return dp[amount]==amount+1?-1:dp[amount];
     }
 }
+
+//More intuitive method
+class Solution {
+    public int coinsFind(int index, int amount, int[] coins, HashMap<String,Integer>memo){
+        if(index==0){
+            if(amount%coins[0]==0){
+                return amount/coins[0];
+            }else{
+                return Integer.MAX_VALUE;
+            }
+        }
+        String currentState = index+" "+amount;
+        if(memo.containsKey(currentState)) 
+            return memo.get(currentState);
+        int notTake = coinsFind(index-1,amount,coins,memo);
+        int take = Integer.MAX_VALUE; 
+        if(coins[index]<=amount){
+            int takeResult = coinsFind(index,amount-coins[index],coins,memo);
+            if(takeResult!=Integer.MAX_VALUE) take = 1+takeResult;
+        }
+        int result =  Math.min(take,notTake);
+        memo.put(currentState,result);
+        return result;
+    }
+    public int coinChange(int[] coins, int amount) {
+        HashMap<String,Integer>memo = new HashMap<>();
+        int n = coins.length;
+        int ans = coinsFind(n-1,amount,coins,memo);
+        return ans == Integer.MAX_VALUE ? -1 : ans;
+    }
+}
