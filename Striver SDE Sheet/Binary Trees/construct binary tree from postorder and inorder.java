11bc66c6c0b69d4de2e2b8b777d81fc8c47fa24a
db-surya <suryastarr123@gmail.com>
2024-12-19 22:33:56 +0530

    fix: prettier fix for  binary tree sum

--- Striver SDE Sheet/Binary Trees/construct binary tree from postorder and inorder.java
+++ Striver SDE Sheet/Binary Trees/construct binary tree from postorder and inorder.java
@@ -1,19 +1,23 @@
-class Solution {
+   class Solution {
     public TreeNode buildTree(int[] inorder, int[] postorder) {
         HashMap<Integer,Integer>map = new HashMap<>();
         for(int i=0;i<inorder.length;i++){
             map.put(inorder[i],i);
         }
-        TreeNode root = inorderPostorder(inorder,0,inorder.length-1,postorder,0,postorder.length-1,map);
+        TreeNode root = inorderPostorder(inorder,0,inorder.length-1,postorder,
+        0,postorder.length-1,map);
         return root;
     }
-    public TreeNode inorderPostorder(int[] inorder, int inStart, int inEnd, int[] postorder, int postStart, int postEnd, HashMap<Integer,Integer>map){
+    public TreeNode inorderPostorder(int[] inorder, int inStart, int inEnd, 
+    int[] postorder, int postStart, int postEnd, HashMap<Integer,Integer>map){
         if(inStart>inEnd || postStart>postEnd) return null;
         TreeNode root = new TreeNode(postorder[postEnd]);
         int inRoot = map.get(postorder[postEnd]);
         int numsLeft = inRoot-inStart;
-        root.left = inorderPostorder(inorder,inStart,inRoot-1,postorder,postStart,postStart+numsLeft-1,map);
-        root.right = inorderPostorder(inorder,inRoot+1,inEnd,postorder,postStart+numsLeft,postEnd-1,map);
+        root.left = inorderPostorder(inorder,inStart,inRoot-1,postorder,
+        postStart,postStart+numsLeft-1,map);
+        root.right = inorderPostorder(inorder,inRoot+1,inEnd,postorder,
+        postStart+numsLeft,postEnd-1,map);
         return root;
     }
 }
